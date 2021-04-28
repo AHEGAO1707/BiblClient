d62ede4c2e9e206a48bfa8c16e63ea65265818ff
Enotello <48958818+EnotelloR@users.noreply.github.com>
2021-04-28 21:13:37 +0600

    Оформлена страница с личными заказами

--- src/components/CardOrder/index.tsx
+++ src/components/CardOrder/index.tsx
@@ -4,6 +4,7 @@ import "../../css/card-book.css"
 
 type PropsType = {
     item: {
+        order_id : number,
         book: {
             title: string
             author: string
@@ -11,30 +12,30 @@ type PropsType = {
             returning_date: string | null
             worker_id: string | number
             taking_date: string
-        }
+        },
+        status: string;
     },
     key: any
 }
 
 const CardBook: React.FC<PropsType> = ({
                                            item,
-                                           key}) => {
+                                           key
+                                       }) => {
 
     return (
-        <div className="card-book" key={key}>
+        <div className="card-book card-book_a" key={key}>
             <Card title={item.book.title} bordered={false}>
+                <p>
+                    Номер заказа: {item.order_id}
+                </p>
                 <p>
                     Автор: {item.book.author}
                 </p>
-                <p>
-                    Год издания: {item.book.date_of_publishing}
-                </p>
-                <p>
-                    Библиотека: {item.book.worker_id}
-                </p>
-                <p>
-                    Вернуть до: {item.book.returning_date}
-                </p>
+                {
+                    item.status === "active" ? (<p style={{color: '#55B432'}}>Активный</p>) :
+                        <p style={{color: "lightsalmon"}}>В архиве</p>
+                }
             </Card>
         </div>
     )
--- src/page/MyBooks/index.tsx
+++ src/page/MyBooks/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { getOrdersRequest } from "../../api/books";
+import {getOrdersRequest} from "../../api/books";
 import CardOrder from "../../components/CardOrder";
 import Spinner from "../../components/Spinner";
 import "./index.css"
@@ -17,15 +17,26 @@ const MyBooks = () => {
 
     return (
         <div className={"ListBook"}>
-            <div className="list-book-cards list-my-book-cards">
-                <Spinner status={listOrders.length > 0}>
+            <Spinner status={listOrders.length > 0}>
+                <h1 className="my-book-header">Активные заказы</h1>
+                <div className="list-book-cards list-my-book-cards">
                     {
                         listOrders.map((item: any) => (
-                            <CardOrder item={item} key={item.taking_date}/>
+                            item.status === "active" ?
+                                <CardOrder item={item} key={item.taking_date}/> : null
                         ))
                     }
-                </Spinner>
-            </div>
+                </div>
+                <h1 className="my-book-header">Заказы в архиве</h1>
+                <div className="list-book-cards list-my-book-cards">
+                    {
+                        listOrders.map((item: any) => (
+                            item.status === "archive" ?
+                                <CardOrder item={item} key={item.taking_date}/> : null
+                        ))
+                    }
+                </div>
+            </Spinner>
         </div>
     )
 }
